e06a30872553313288ba6ad21eed430f297a9d65
SalmanAziz <sheikhsalman91@live.com>
2023-04-17 16:39:36 +0500

    commit

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { SellerService } from '../services/seller.service';
 
 @Component({
   selector: 'app-seller-auth',
   templateUrl: './seller-auth.component.html',
   styleUrls: ['./seller-auth.component.css']
 })
-export class SellerAuthComponent {
+export class SellerAuthComponent implements OnInit{
 
 
+  constructor(private seller:SellerService ){}
 
+  ngOnInit(): void {}
   signUp(data:Object):void{
-    console.log(data);
-  }
+   console.log(data);
 
+   this.seller.userSignup(data).subscribe((result)=>{
+  console.log(result);
+   });
+  }
 }
